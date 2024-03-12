1e97e0e9ffbee8a3c7c9d6adb7d36e88cbe933a2
emilianogastaldo <emiliano0303@gmail.com>
2024-03-12 16:32:44 +0100

    added function

--- app/Models/train.php
+++ app/Models/train.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Train extends Model
 {
     use HasFactory;
+
+    public function getInTime()
+    {
+        return $this->in_time ? 'Si' : 'No';
+    }
+
+    public function getDeleted()
+    {
+        return $this->deleted ? 'Cancellato' : 'In arrivo';
+    }
 }
